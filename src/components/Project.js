67d669d20f5f2e2b3b0da79be48273c6a1753d10
BhanuPratap0 <119663548+BhanuPratap0@users.noreply.github.com>
2024-01-14 12:13:28 +0530

    Social Media Added

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,12 +3,30 @@ import React from 'react'
 const Project = () => {
     return (
         <div id='projects'>
-            <div  className='projects-body' >
+            <div className='projects-body' >
                 <div className='edu-head' >
                     <img className='project-head' src={require('./images/projects.png')} />
                     <h1 style={{ marginTop: "20px" }} ><b>My <span style={{ color: "#126a60" }} >Projects</span></b></h1>
                 </div>
                 <div className='projects'>
+
+                    <div className='containerx-parent'>
+                        <h3></h3>
+                        <div class="containerx">
+                            <img src={require('./images/sociosync.jpeg')} alt="Avatar" class="image"   />
+                            <div class="overlay">
+                                <h3>Social Media App MERN</h3>
+                                <div class="text"><a href='https://sociosync.netlify.app/' target='_blank' > <i class="fa-regular fa-eye" style={{ marginRight: "5px" }} ></i>SocioSync - Social Media App</a></div>
+                                <a href='https://github.com/BhanuPratap0/inotebook' ><div className='button-class' >
+                                    <i class="fa-solid fa-code" style={{ color: "#ffffff", marginTop: "5px", marginLeft: "5px", marginRight: "-5px target='_blank'" }}></i>
+                                    <button className='btn btn-primary btn-sm mx-1 code-button '>code</button>
+                                </div>
+                                </a>
+                                <p>Full-Stack Notes-App Built Using MERN Stack. Easy To Use And Useful For Day-To-Day Note Making</p>
+                            </div>
+                        </div>
+                    </div>
+
                     <div className='containerx-parent'>
                         <h3></h3>
                         <div class="containerx">
